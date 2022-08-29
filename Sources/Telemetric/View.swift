6ac58034bf480e4241c645de4bad6109c2b07a26
Jordan Kay <jordan@fleuronic.com>
2022-08-28 22:06:06 -0400

    Add visible and hidden bindings

--- Sources/Telemetric/View.swift
+++ Sources/Telemetric/View.swift
@@ -6,6 +6,16 @@ import Bond
 import Metric
 
 public extension UIView {
+	func visible<Source: SignalProtocol>(_ source: Source) -> Self where Source.Element == Bool, Source.Error == Never {
+		_ = source.bind(to: reactive.isVisible)
+		return self
+	}
+
+	func hidden<Source: SignalProtocol>(_ source: Source) -> Self where Source.Element == Bool, Source.Error == Never {
+		_ = source.bind(to: reactive.isHidden)
+		return self
+	}
+
 	func opacity<Source: SignalProtocol>(_ source: Source) -> Self where Source.Element == Opacity, Source.Error == Never {
 		_ = source.bind(to: reactive.opacity)
 		return self
@@ -14,6 +24,10 @@ public extension UIView {
 
 // MARK: -
 private extension ReactiveExtensions where Base: UIView {
+	var isVisible: Bond<Bool> {
+		bond { $0.isHidden = !$1 }
+	}
+
 	var opacity: Bond<Opacity> {
 		bond { $0.opacity = $1 }
 	}
