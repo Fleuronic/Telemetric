6b590671608aa0128e171b7b70e8b350ce5fc366
Jordan Kay <jordan@fleuronic.com>
2022-12-12 11:11:59 -0500

    Add max length

--- Sources/Telemetric/Text/TextField.swift
+++ Sources/Telemetric/Text/TextField.swift
@@ -11,7 +11,8 @@ import protocol ReactiveKit.SignalProtocol
 import protocol ReactiveKit.ReactiveExtensions
 
 public final class TextField: UITextField {
-	public var acceptedCharacter: Regex<AnyRegexOutput>?
+	public var maxLength: Int?
+	public var acceptedCharacter: Regex<Substring>?
 
 	public override init(frame: CGRect) {
 		super.init(frame: frame)
@@ -26,8 +27,15 @@ public final class TextField: UITextField {
 // MARK: -
 extension TextField: UITextFieldDelegate {
 	public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-		guard !string.isEmpty else { return true }
-		return acceptedCharacter.map(string.wholeMatch).map { $0 != nil } ?? true
+		guard
+			!string.isEmpty,
+			let text = textField.text,
+			let range = Range(range, in: text) else { return true }
+
+		let result = text.replacingCharacters(in: range, with: string)
+		return
+			maxLength.map { result.count <= $0 } ?? true &&
+			acceptedCharacter.map(string.wholeMatch).map { $0 != nil } ?? true
 	}
 }
 
