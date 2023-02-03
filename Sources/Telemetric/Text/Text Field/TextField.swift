92eedd7b13e98c2ecb0cf6687943cf90dc6d769c
Jordan Kay <jordan@fleuronic.com>
2023-02-03 16:31:41 -0500

    Support hiding cursor in text field

--- Sources/Telemetric/Text/Text Field/TextField.swift
+++ Sources/Telemetric/Text/Text Field/TextField.swift
@@ -1,11 +1,13 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-import class UIKit.UITextField
-import class Foundation.NSCoder
-import struct Foundation.NSRange
 import struct UIKit.CGRect
 import struct Bond.Bond
 import struct ReactiveKit.SafeSignal
+import class UIKit.UIView
+import class UIKit.UITextField
+import class UIKit.UITextPosition
+import class Foundation.NSCoder
+import struct Foundation.NSRange
 import protocol UIKit.UITextFieldDelegate
 import protocol ReactiveKit.SignalProtocol
 import protocol ReactiveKit.ReactiveExtensions
@@ -14,13 +16,45 @@ public final class TextField: UITextField {
 	public var maxLength: Int?
 	public var acceptedCharacter: Regex<Substring>?
 
+	public var showsCursor = true {
+		didSet {
+			if showsCursor {
+				coverView.removeFromSuperview()
+			} else {
+				addSubview(coverView)
+			}
+		}
+	}
+
+	private lazy var coverView: UIView = {
+		let view = UIView()
+		addSubview(view)
+		return view
+	}()
+
+	// MARK: NSCoding
+	required init(coder: NSCoder) {
+		fatalError()
+	}
+
+	// MARK: UIView
 	public override init(frame: CGRect) {
 		super.init(frame: frame)
 		self.delegate = self
 	}
 
-	required init(coder: NSCoder) {
-		fatalError()
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+
+		if !showsCursor {
+			coverView.frame = bounds
+			bringSubviewToFront(coverView)
+		}
+	}
+
+	// MARK: UITextInput
+	public override func caretRect(for position: UITextPosition) -> CGRect {
+		showsCursor ? super.caretRect(for: position) : .zero
 	}
 }
 
