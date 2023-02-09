4ede76b273c2f121055551759fa08175acb166da
Jordan Kay <jordan@fleuronic.com>
2023-02-08 19:29:51 -0500

    Clean up TextField

--- Sources/Telemetric/Text/Text Field/TextField.swift
+++ Sources/Telemetric/Text/Text Field/TextField.swift
@@ -3,37 +3,19 @@
 import struct UIKit.CGRect
 import struct Bond.Bond
 import struct ReactiveKit.SafeSignal
-import class UIKit.UIView
 import class UIKit.UITextField
-import class UIKit.UITextPosition
 import class Foundation.NSCoder
 import struct Foundation.NSRange
 import protocol UIKit.UITextFieldDelegate
 import protocol ReactiveKit.SignalProtocol
 import protocol ReactiveKit.ReactiveExtensions
 
-public final class TextField: UITextField {
+open class TextField: UITextField {
 	public var maxLength: Int?
 	public var acceptedCharacter: Regex<Substring>?
 
-	public var showsCursor = true {
-		didSet {
-			if showsCursor {
-				coverView.removeFromSuperview()
-			} else {
-				addSubview(coverView)
-			}
-		}
-	}
-
-	private lazy var coverView: UIView = {
-		let view = UIView()
-		addSubview(view)
-		return view
-	}()
-
 	// MARK: NSCoding
-	required init(coder: NSCoder) {
+	public required init(coder: NSCoder) {
 		fatalError()
 	}
 
@@ -41,20 +23,6 @@ public final class TextField: UITextField {
 	public override init(frame: CGRect) {
 		super.init(frame: frame)
 		self.delegate = self
-	}
-
-	public override func layoutSubviews() {
-		super.layoutSubviews()
-
-		if !showsCursor {
-			coverView.frame = bounds
-			bringSubviewToFront(coverView)
-		}
-	}
-
-	// MARK: UITextInput
-	public override func caretRect(for position: UITextPosition) -> CGRect {
-		showsCursor ? super.caretRect(for: position) : .zero
 	}
 }
 
